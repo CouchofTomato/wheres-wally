3da1f74747ff709a022aa738f81207a1b7b9e3b0
Austin Mason <austinpmason@gmail.com>
2017-09-28 12:42:44 +0000

    add factory to photo specs

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  subject(:photo) do
-    Photo.new(
-      url: 'awesome.jpg',
-      title: 'You wish you were me'
-    )
-  end
+  subject(:photo) { build(:photo) }
 
   it { is_expected.to validate_presence_of(:url) }
   it { is_expected.to validate_presence_of(:title) }
