4143cab884c861642c9f6d2ad2d2ef2574edd50c
Austin Mason <austinpmason@gmail.com>
2017-09-29 12:51:54 +0000

    create photos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :photo, only: [:index, :show] do
     resource :score
