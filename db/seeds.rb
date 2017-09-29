3c7317a3540095e922035469f27c6c6ccab60247
Austin Mason <austinpmason@gmail.com>
2017-09-29 09:27:26 +0000

    add on the beach solutions to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,13 @@ Character.create(
     {name: 'Woof'}
   ]
 )
+
+beach = Photo.find_by(title: 'On the Beach')
+character = Character.find_by(name: 'Waldo')
+PhotosCharacter.create(photo: beach, character: character, top_x: 885, top_y: 314, bottom_x: 908, bottom_y: 358)
+character = Character.find_by(name: 'Odlaw')
+PhotosCharacter.create(photo: beach, character: character, top_x: 132, top_y: 293, bottom_x: 141, bottom_y: 321)
+character = Character.find_by(name: 'Wenda')
+PhotosCharacter.create(photo: beach, character: character, top_x: 1118, top_y: 347, bottom_x: 1132, bottom_y: 366)
+character = Character.find_by(name: 'Wizard Whitebeard')
+PhotosCharacter.create(photo: beach, character: character, top_x: 373, top_y: 392, bottom_x: 388, bottom_y: 327)
