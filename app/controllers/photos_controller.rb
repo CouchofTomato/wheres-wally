57707f385344a375b669c43d0b6d21a88c2e07b2
Austin Mason <austinpmason@gmail.com>
2017-10-02 11:07:36 +0000

    write index and show photos tests

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,9 @@
 class PhotosController < ApplicationController
   def index
+    @photo = Photo.all
   end
 
   def show
+    
   end
 end
--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PhotosController, type: :controller do
+  let(:photos) { double 'Photos' }
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
+    
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
+  describe 'GET #show' do
+    it 'returns http success' do
       get :show
       expect(response).to have_http_status(:success)
     end
   end
-
 end
