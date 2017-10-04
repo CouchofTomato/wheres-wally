88e24e393958888394852c0cb2d0dbc93ec63eb7
Austin Mason <austinpmason@gmail.com>
2017-10-04 15:23:37 +0000

    write score controller tests

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,7 +1,10 @@
 class ScoresController < ApplicationController
   def index
+    @photo = Photo.find(params[:photo_id])
+    @scores = @photo.scores
   end
   
   def create
+    @photo = Photo.find(params[:photo_id])
   end
 end
--- spec/controllers/scores_controller_spec.rb
+++ spec/controllers/scores_controller_spec.rb
@@ -1,18 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe ScoresController, type: :controller do
+  let(:photo) { double 'Photo' }
+  let(:score) { double 'Score' }
+  let(:scores) { [1, 2, 3] }
+  let(:params) { { id: '2',  photo_id: photo } }
+
+  before(:each) do
+    allow(Photo).to receive(:find).and_return(photo)
+    allow(photo).to receive(:scores).and_return(scores)
+  end
+
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :index, params: params
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get :index, params: params
+      expect(response).to render_template :index
+    end
+
+    it 'assigns the scores for that photo to @scores' do
+      get :index, params: params
+      expect(assigns(:scores)).to match_array([1, 2, 3])
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe '#create' do
+    it 'creates a new score for the photo' do
+      post :create, params: params
     end
   end
-
 end
