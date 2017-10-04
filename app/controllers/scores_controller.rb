3942798df2b3b651c8fb5e79d6cfa88aeef55ef6
Austin Mason <austinpmason@gmail.com>
2017-10-04 11:10:20 +0000

    create Scores controller

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,10 @@
+class ScoresController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root 'photos#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :photos, only: [:index, :show] do
-    resource :scores, only: [:create, :show]
+    resource :scores, only: [:new, :create, :show]
   end
 end
